ca44b6a9ac44fb3c512b42e01a30e1a45f8fb102
Freewind <nowindlee@gmail.com>
2023-09-25 18:37:59 +0800

    upgrade to latest version

--- src/main.tsx
+++ src/main.tsx
@@ -1,226 +1,292 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
 import {
-  createTable,
+  Column,
+  Table as ReactTable,
   PaginationState,
-  useTableInstance,
+  useReactTable,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  ColumnDef,
+  OnChangeFn,
+  flexRender,
 } from '@tanstack/react-table'
 
-//
-
-import { fetchData, Person } from './fetchData'
-
-const queryClient = new QueryClient()
-
-let table = createTable().setRowType<Person>()
+import { makeData, Person } from './makeData'
 
 function App() {
   const rerender = React.useReducer(() => ({}), {})[1]
 
-  const columns = React.useMemo(
-    () => [
-      table.createGroup({
-        header: 'Name',
-        footer: props => props.column.id,
-        columns: [
-          table.createDataColumn('firstName', {
-            cell: info => info.getValue(),
-            footer: props => props.column.id,
-          }),
-          table.createDataColumn(row => row.lastName, {
-            id: 'lastName',
-            cell: info => info.getValue(),
-            header: () => <span>Last Name</span>,
-            footer: props => props.column.id,
-          }),
-        ],
-      }),
-      table.createGroup({
-        header: 'Info',
-        footer: props => props.column.id,
-        columns: [
-          table.createDataColumn('age', {
-            header: () => 'Age',
-            footer: props => props.column.id,
-          }),
-          table.createGroup({
-            header: 'More Info',
-            columns: [
-              table.createDataColumn('visits', {
-                header: () => <span>Visits</span>,
-                footer: props => props.column.id,
-              }),
-              table.createDataColumn('status', {
-                header: 'Status',
-                footer: props => props.column.id,
-              }),
-              table.createDataColumn('progress', {
-                header: 'Profile Progress',
-                footer: props => props.column.id,
-              }),
-            ],
-          }),
-        ],
-      }),
-    ],
-    []
+  const columns = React.useMemo<ColumnDef<Person>[]>(
+      () => [
+        {
+          header: 'Name',
+          footer: props => props.column.id,
+          columns: [
+            {
+              accessorKey: 'firstName',
+              cell: info => info.getValue(),
+              footer: props => props.column.id,
+            },
+            {
+              accessorFn: row => row.lastName,
+              id: 'lastName',
+              cell: info => info.getValue(),
+              header: () => <span>Last Name</span>,
+              footer: props => props.column.id,
+            },
+          ],
+        },
+        {
+          header: 'Info',
+          footer: props => props.column.id,
+          columns: [
+            {
+              accessorKey: 'age',
+              header: () => 'Age',
+              footer: props => props.column.id,
+            },
+            {
+              header: 'More Info',
+              columns: [
+                {
+                  accessorKey: 'visits',
+                  header: () => <span>Visits</span>,
+                  footer: props => props.column.id,
+                },
+                {
+                  accessorKey: 'status',
+                  header: 'Status',
+                  footer: props => props.column.id,
+                },
+                {
+                  accessorKey: 'progress',
+                  header: 'Profile Progress',
+                  footer: props => props.column.id,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      []
   )
 
-  const [{ pageIndex, pageSize }, setPagination] =
-    React.useState<PaginationState>({
-      pageIndex: 0,
-      pageSize: 10,
-    })
+  const [data, setData] = React.useState(() => makeData(100000))
+  const refreshData = () => setData(() => makeData(100000))
 
-  const fetchDataOptions = {
-    pageIndex,
-    pageSize,
-  }
-
-  const dataQuery = useQuery(
-    ['data', fetchDataOptions],
-    () => fetchData(fetchDataOptions),
-    { keepPreviousData: true }
+  return (
+      <>
+        <Table
+            {...{
+              data,
+              columns,
+            }}
+        />
+        <hr />
+        <div>
+          <button onClick={() => rerender()}>Force Rerender</button>
+        </div>
+        <div>
+          <button onClick={() => refreshData()}>Refresh Data</button>
+        </div>
+      </>
   )
+}
 
-  const defaultData = React.useMemo(() => [], [])
-
-  const pagination = React.useMemo(
-    () => ({
-      pageIndex,
-      pageSize,
-    }),
-    [pageIndex, pageSize]
-  )
-
-  const instance = useTableInstance(table, {
-    data: dataQuery.data?.rows ?? defaultData,
+function Table({
+                 data,
+                 columns,
+               }: {
+  data: Person[]
+  columns: ColumnDef<Person>[]
+}) {
+  const table = useReactTable({
+    data,
     columns,
-    pageCount: dataQuery.data?.pageCount ?? -1,
-    state: {
-      pagination,
-    },
-    onPaginationChange: setPagination,
+    // Pipeline
     getCoreRowModel: getCoreRowModel(),
-    manualPagination: true,
-    // getPaginationRowModel: getPaginationRowModel(), // If only doing manual pagination, you don't need this
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    //
     debugTable: true,
   })
 
   return (
-    <div className="p-2">
-      <div className="h-2" />
-      <table>
-        <thead>
-        {instance.getHeaderGroups().map(headerGroup => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map(header => {
-              return (
-                <th key={header.id} colSpan={header.colSpan}>
-                  {header.isPlaceholder ? null : (
-                    <div>{header.renderHeader()}</div>
-                  )}
-                </th>
-              )
-            })}
-          </tr>
-        ))}
-        </thead>
-        <tbody>
-        {instance.getRowModel().rows.map(row => {
-          return (
-            <tr key={row.id}>
-              {row.getVisibleCells().map(cell => {
-                return <td key={cell.id}>{cell.renderCell()}</td>
-              })}
-            </tr>
-          )
-        })}
-        </tbody>
-      </table>
-      <div className="h-2" />
-      <div className="flex items-center gap-2">
-        <button
-          className="border rounded p-1"
-          onClick={() => instance.setPageIndex(0)}
-          disabled={!instance.getCanPreviousPage()}
-        >
-          {'<<'}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => instance.previousPage()}
-          disabled={!instance.getCanPreviousPage()}
-        >
-          {'<'}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => instance.nextPage()}
-          disabled={!instance.getCanNextPage()}
-        >
-          {'>'}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => instance.setPageIndex(instance.getPageCount() - 1)}
-          disabled={!instance.getCanNextPage()}
-        >
-          {'>>'}
-        </button>
-        <span className="flex items-center gap-1">
+      <div className="p-2">
+        <div className="h-2" />
+        <table>
+          <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                      <th key={header.id} colSpan={header.colSpan}>
+                        {header.isPlaceholder ? null : (
+                            <div>
+                              {flexRender(
+                                  header.column.columnDef.header,
+                                  header.getContext()
+                              )}
+                              {header.column.getCanFilter() ? (
+                                  <div>
+                                    <Filter column={header.column} table={table} />
+                                  </div>
+                              ) : null}
+                            </div>
+                        )}
+                      </th>
+                  )
+                })}
+              </tr>
+          ))}
+          </thead>
+          <tbody>
+          {table.getRowModel().rows.map(row => {
+            return (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map(cell => {
+                    return (
+                        <td key={cell.id}>
+                          {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                          )}
+                        </td>
+                    )
+                  })}
+                </tr>
+            )
+          })}
+          </tbody>
+        </table>
+        <div className="h-2" />
+        <div className="flex items-center gap-2">
+          <button
+              className="border rounded p-1"
+              onClick={() => table.setPageIndex(0)}
+              disabled={!table.getCanPreviousPage()}
+          >
+            {'<<'}
+          </button>
+          <button
+              className="border rounded p-1"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+          >
+            {'<'}
+          </button>
+          <button
+              className="border rounded p-1"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+          >
+            {'>'}
+          </button>
+          <button
+              className="border rounded p-1"
+              onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+              disabled={!table.getCanNextPage()}
+          >
+            {'>>'}
+          </button>
+          <span className="flex items-center gap-1">
           <div>Page</div>
           <strong>
-            {instance.getState().pagination.pageIndex + 1} of{' '}
-            {instance.getPageCount()}
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
           </strong>
         </span>
-        <span className="flex items-center gap-1">
+          <span className="flex items-center gap-1">
           | Go to page:
           <input
-            type="number"
-            defaultValue={instance.getState().pagination.pageIndex + 1}
-            onChange={e => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              instance.setPageIndex(page)
-            }}
-            className="border p-1 rounded w-16"
+              type="number"
+              defaultValue={table.getState().pagination.pageIndex + 1}
+              onChange={e => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                table.setPageIndex(page)
+              }}
+              className="border p-1 rounded w-16"
           />
         </span>
-        <select
-          value={instance.getState().pagination.pageSize}
-          onChange={e => {
-            instance.setPageSize(Number(e.target.value))
-          }}
-        >
-          {[10, 20, 30, 40, 50].map(pageSize => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-        {dataQuery.isFetching ? 'Loading...' : null}
+          <select
+              value={table.getState().pagination.pageSize}
+              onChange={e => {
+                table.setPageSize(Number(e.target.value))
+              }}
+          >
+            {[10, 20, 30, 40, 50].map(pageSize => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+                </option>
+            ))}
+          </select>
+        </div>
+        <div>{table.getRowModel().rows.length} Rows</div>
+        <pre>{JSON.stringify(table.getState().pagination, null, 2)}</pre>
       </div>
-      <div>{instance.getRowModel().rows.length} Rows</div>
-      <div>
-        <button onClick={() => rerender()}>Force Rerender</button>
+  )
+}
+function Filter({
+                  column,
+                  table,
+                }: {
+  column: Column<any, any>
+  table: ReactTable<any>
+}) {
+  const firstValue = table
+      .getPreFilteredRowModel()
+      .flatRows[0]?.getValue(column.id)
+
+  const columnFilterValue = column.getFilterValue()
+
+  return typeof firstValue === 'number' ? (
+      <div className="flex space-x-2">
+        <input
+            type="number"
+            value={(columnFilterValue as [number, number])?.[0] ?? ''}
+            onChange={e =>
+                column.setFilterValue((old: [number, number]) => [
+                  e.target.value,
+                  old?.[1],
+                ])
+            }
+            placeholder={`Min`}
+            className="w-24 border shadow rounded"
+        />
+        <input
+            type="number"
+            value={(columnFilterValue as [number, number])?.[1] ?? ''}
+            onChange={e =>
+                column.setFilterValue((old: [number, number]) => [
+                  old?.[0],
+                  e.target.value,
+                ])
+            }
+            placeholder={`Max`}
+            className="w-24 border shadow rounded"
+        />
       </div>
-      <pre>{JSON.stringify(pagination, null, 2)}</pre>
-    </div>
+  ) : (
+      <input
+          type="text"
+          value={(columnFilterValue ?? '') as string}
+          onChange={e => column.setFilterValue(e.target.value)}
+          placeholder={`Search...`}
+          className="w-36 border shadow rounded"
+      />
   )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Failed to find the root element')
+
+ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
       <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    </React.StrictMode>
 )
